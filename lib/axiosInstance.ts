141fa21fb44761f5d8b6fcbdc416d95eb4618d89
frankl <ramosfp99@gmail.com>
2025-02-15 17:33:08 +0800

    updates

--- lib/axiosInstance.ts
+++ lib/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from 'axios';
 
 const axiosInstance: AxiosInstance = axios.create({
-    baseURL: 'http://localhost:3000/api', // Replace with your API base URL
+    baseURL: 'https://franklinramos.vercel.app/api',
     headers: {
       'Content-Type': 'application/json',
     },
@@ -9,19 +9,16 @@ const axiosInstance: AxiosInstance = axios.create({
 
 axiosInstance.interceptors.request.use(
     (config) => {
-        // Do something before the request is sent
         console.log('Request:', config);
         return config;
     },
     (error) => {
-        // Handle the error
         return Promise.reject(error);
     }
 );
 
 axiosInstance.interceptors.response.use(
     (response) => {
-        // Do something with response data
         console.log('Axios response: -->', response)
         return response;
     },
