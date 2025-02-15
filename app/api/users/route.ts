b90747e46aeee42b11ff9fa5a1538d5b1837cbf9
frankl <ramosfp99@gmail.com>
2025-02-15 16:00:00 +0800

    updating files

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,10 +1,17 @@
-import prisma from "@/lib/prisma";
+import { User } from "@/models/schema";
+import { connectDB } from "@/lib/db";
+import { NextResponse } from "next/server";
 
-export async function GET() {
-    const users = await prisma.user.findMany();
-    return new Response(JSON.stringify(users), {
-        headers: {
-            "content-type": "application",
-        }
-    });
+export async function GET(): Promise<NextResponse> {
+  try {
+    await connectDB();
+    const users = await User.find({}, "_id name");
+    return NextResponse.json({ users }, { status: 200 });
+  } catch (error) {
+    console.error("Error fetching users:", error);
+    return NextResponse.json(
+      { message: "Failed to fetch users" },
+      { status: 500 }
+    );
+  }
 }
