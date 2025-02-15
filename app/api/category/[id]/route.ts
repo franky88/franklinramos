194b0fed854f87dfacefd75fce708aa1748d45fe
frankl <ramosfp99@gmail.com>
2025-02-15 16:05:44 +0800

    updating files

--- app/api/category/[id]/route.ts
+++ app/api/category/[id]/route.ts
@@ -2,20 +2,24 @@ import { NextRequest, NextResponse } from "next/server";
 import { connectDB } from "@/lib/db";
 import { Category } from "@/models/schema";
 
-export async function GET(request: NextRequest, context: { params: { _id: string } }): Promise<NextResponse> {
+export async function GET(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+): Promise<NextResponse> {
   try {
     await connectDB();
-    
-    const { _id } = context.params;
-    console.log("response from the server ID: ", _id)
-    const category = await Category.findById(_id).select("_id name");
-    
+
+    const { id } = params;
+
+    const category = await Category.findById(id).select("_id name");
+
     if (!category) {
       return NextResponse.json(
-        { message: `Category with id ${_id} not found` },
+        { message: `Category with id ${id} not found` },
         { status: 404 }
       );
     }
+
     return NextResponse.json({ category }, { status: 200 });
   } catch (error) {
     console.error("Error fetching category:", error);
