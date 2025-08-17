07f2e0b8cf913376c3a03d9a0ef457317722ad89
Franklin Ramos <ramosfp99@gmail.com>
2025-08-17 10:43:22 +0800

    portfolio update

---
+++ src/constants/images.ts
@@ -0,0 +1,35 @@
+import image01 from "../../public/images/bioflora_icwyzm.jpg";
+import image02 from "../../public/images/Bostech_au2vmk.jpg";
+import image03 from "../../public/images/cvp_thumb_kk05lw.jpg";
+import image04 from "../../public/images/dailydose_jnci7y.jpg";
+import image05 from "../../public/images/Desktop_-_1_1_vysoen.jpg";
+import image06 from "../../public/images/direct_t0brgx.jpg";
+import image07 from "../../public/images/djm_wood_ap3w00.jpg";
+import image08 from "../../public/images/Gen-Eco-Animated-Logo-v2_ml563j.gif";
+import image09 from "../../public/images/integrity_fwtney.jpg";
+import image10 from "../../public/images/LMCG-logo-V2_1_jet3m8.gif";
+import image11 from "../../public/images/podiatry_kjqhfm.jpg";
+import image12 from "../../public/images/prorepair_ojjx5w.jpg";
+import image13 from "../../public/images/signarama_oo3xxw.jpg";
+import image14 from "../../public/images/truckmech_brw7i0.png";
+import image15 from "../../public/images/yourtaxmate_ygojzc.jpg";
+
+const images = {
+  image01,
+  image02,
+  image03,
+  image04,
+  image05,
+  image06,
+  image07,
+  image08,
+  image09,
+  image10,
+  image11,
+  image12,
+  image13,
+  image14,
+  image15,
+};
+
+export default images;
